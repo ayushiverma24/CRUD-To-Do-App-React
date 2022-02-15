e2821cf7c6f14f004299bae159523c191ec3c182
AYUSHI VERMA <vayushi121@gmail.com>
2022-02-15 20:19:58 +0530

    Added svg image

--- to_do_list/src/Components/Home/Home.js
+++ to_do_list/src/Components/Home/Home.js
@@ -2,15 +2,17 @@ import React, { Component } from "react";
 import "./Home.css";
 import TextField from "@mui/material/TextField";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import InputAdornment from "@mui/material/InputAdornment";
+import Image1 from "../Images/Image1.svg";
+
 export class Home extends Component {
   render() {
     return (
       <div className="thePage">
         <div className="myGlass">
+          <img className="theImage" src={Image1}></img>
+
           <div className="myList">
-            <h1>To Do List</h1>
+            <h1 align="right">To Do List</h1>
             <div className="myInput">
               <TextField label="Task" variant="outlined" fullWidth />
               <AddCircleOutlineRoundedIcon fontSize="large" />
