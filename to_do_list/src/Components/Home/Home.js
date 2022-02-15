3c456fc9e0c6d08dcb4d62e45dfaae272b429abc
AYUSHI VERMA <vayushi121@gmail.com>
2022-02-15 20:39:23 +0530

    Image final

--- to_do_list/src/Components/Home/Home.js
+++ to_do_list/src/Components/Home/Home.js
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 import "./Home.css";
 import TextField from "@mui/material/TextField";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
-import Image1 from "../Images/Image1.svg";
+import Image3 from "../Images/Image3.svg";
 
 export class Home extends Component {
   render() {
     return (
       <div className="thePage">
         <div className="myGlass">
-          <img className="theImage" src={Image1}></img>
+          <img className="theImage" src={Image3}></img>
 
           <div className="myList">
             <h1 align="right">To Do List</h1>
             <div className="myInput">
               <TextField label="Task" variant="outlined" fullWidth />
-              <AddCircleOutlineRoundedIcon fontSize="large" />
+              <AddCircleOutlineRoundedIcon
+                className="addIcon"
+                fontSize="large"
+              />
             </div>
           </div>
         </div>
