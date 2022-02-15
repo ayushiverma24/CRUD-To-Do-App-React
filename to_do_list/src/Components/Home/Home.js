d68f4a5bdd3575125e96ae6a2d4e49fb904e1c57
AYUSHI VERMA <vayushi121@gmail.com>
2022-02-15 19:48:39 +0530

    Initial commit

---
+++ to_do_list/src/Components/Home/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Home.css";
+import TextField from "@mui/material/TextField";
+import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+export class Home extends Component {
+  render() {
+    return (
+      <div className="thePage">
+        <div className="myGlass">
+          <div className="myList">
+            <h1>To Do List</h1>
+            <div className="myInput">
+              <TextField label="Task" variant="outlined" fullWidth />
+              <AddCircleOutlineRoundedIcon fontSize="large" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
