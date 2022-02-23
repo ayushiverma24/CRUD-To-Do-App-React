ce340991a9ca68a4734b782f3ba1f526029f2b63
AYUSHI VERMA <vayushi121@gmail.com>
2022-02-23 17:48:16 +0530

    Changed list

--- to_do_list/src/Components/Home/Home.js
+++ to_do_list/src/Components/Home/Home.js
@@ -4,6 +4,8 @@ import TextField from "@mui/material/TextField";
 import AddCircleOutlineRoundedIcon from "@mui/icons-material/AddCircleOutlineRounded";
 import Image3 from "../Images/Image3.svg";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import EditIcon from "@mui/icons-material/Edit";
+
 export class Home extends Component {
   constructor(props) {
     super(props);
@@ -11,35 +13,88 @@ export class Home extends Component {
     this.state = {
       task: "",
       taskList: [],
+      flag: false,
+      address: 0,
+      currentTask: {
+        task: "",
+        id: "",
+      },
     };
     this.handleInput = this.handleInput.bind(this);
     this.handleAddTask = this.handleAddTask.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
+    this.handleUpdateTask = this.handleUpdateTask.bind(this);
   }
   handleInput = (e) => {
-    this.setState({
-      task: e.target.value,
-    });
+    if (e.target.value.trim().length === 0) {
+    } else {
+      this.setState({
+        currentTask: {
+          task: e.target.value,
+          id: Date.now(),
+        },
+      });
+    }
   };
   handleAddTask = (e) => {
     e.preventDefault();
-    this.setState({
-      taskList: this.state.taskList.concat(this.state.task),
-    });
-    this.setState({
-      task: "",
-    });
+    const newTask = this.state.currentTask;
+    if (newTask.task !== "") {
+      const newTaskList = [...this.state.taskList, newTask];
+      this.setState({
+        taskList: newTaskList,
+        currentTask: {
+          task: "",
+          key: "",
+        },
+      });
+    }
+    // if (this.state.flag) {
+    //   this.state.taskList.map((data, key) => {
+    //     if (this.state.address === key) {
+    //       console.log("data inside = ", data);
+    //       console.log("inside this.state.task = ", this.state.task);
+    //       // this.setState({
+    //       //   taskList[key]: this.state.task
+    //       // })
+    //       return this.state.task;
+    //     }
+    //   });
+    // } else {
+    //   if()
+    //   this.setState({
+    //     taskList: this.state.taskList.concat(this.state.task),
+    //   });
+    //   this.setState({
+    //     task: "",
+    //   });
+    // }
+
+    // if()
   };
   handleDelete = (i) => {
     const newList = this.state.taskList.filter((data, index) => {
-      return i !== index;
+      return i !== index; // returning those data which are not matching with "i"
     });
     this.setState({
       taskList: newList,
     });
   };
+  handleUpdateTask = () => {
+    console.log("clicked");
+  };
+  // handleEdit = (i) => {
+  //   console.log("slected = ", i);
+  //   var item = this.state.taskList[i];
+  //   this.setState({
+  //     task: item,
+  //     address: i,
+  //     flag: true,
+  //   });
+  // };
   render() {
     let data = this.state.taskList;
+    console.log("data = ", data);
+    console.log("this.state = ", this.state);
     return (
       <div className="thePage">
         <div className="myGlass">
@@ -54,7 +109,7 @@ export class Home extends Component {
                   variant="outlined"
                   fullWidth
                   required
-                  value={this.state.task}
+                  value={this.state.currentTask.task}
                   onChange={(e) => this.handleInput(e)}
                 />
                 <AddCircleOutlineRoundedIcon
@@ -69,9 +124,32 @@ export class Home extends Component {
               {this.state.taskList.length > 0 ? (
                 <div>
                   {data.map((myData, i) => {
+                    {
+                      console.log("myData = ", myData);
+                    }
+
                     return (
-                      <div className="theList">
-                        <p>{myData}</p>
+                      <div className="theList" key={i}>
+                        <TextField
+                          variant="outlined"
+                          fullWidth
+                          required
+                          className="listInput"
+                          value={myData.task}
+                          onChange={this.handleUpdateTask}
+                        />
+                        {/* <input
+                          type="text"
+                          id={myData.id}
+                          value={myData.task}
+                        ></input> */}
+                        {/* <p>{myData.task}</p> */}
+                        {/* <EditIcon
+                          fontSize="small"
+                          onClick={() => {
+                            this.handleEdit(i);
+                          }}
+                        /> */}
                         <DeleteForeverIcon
                           className="delIcon"
                           fontSize="small"
